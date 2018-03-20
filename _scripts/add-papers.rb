e3f8d1740a254486a71858ec0091bed826ad0f64
Spencer Vatrt-Watts <asvw@uvic.ca>
2018-03-20 11:34:49 -0700

    Add Altmetric badges to paper pages

--- _scripts/add-papers.rb
+++ _scripts/add-papers.rb
@@ -9,8 +9,6 @@ xml.xpath('//PubmedArticle').each do |article|
 	file_name = article.xpath('PubmedData/History/PubMedPubDate[@PubStatus="pubmed"]/Year').text + "-" + article.xpath('PubmedData/History/PubMedPubDate[@PubStatus="pubmed"]/Month').text + "-" + article.xpath('PubmedData/History/PubMedPubDate[@PubStatus="pubmed"]/Day').text + "-" + article.xpath('PubmedData/ArticleIdList/ArticleId[@IdType="pubmed"]').text
 	
 	file = File.new("papers/_posts/" + file_name + ".md", "w")
-	
-	# Pull all other required data from assets/pubmed_result.xml
 
 	file.puts("---")
 	file.puts("layout: paper")
@@ -29,28 +27,38 @@ xml.xpath('//PubmedArticle').each do |article|
 	file.puts("")
 
 	file.puts("ref: " + article.xpath('MedlineCitation/Article/AuthorList/Author/LastName')[0].text + " et al. " + article.xpath('MedlineCitation/Article/Journal/JournalIssue/PubDate/Year').text + ". " + article.xpath('MedlineCitation/Article/Journal/ISOAbbreviation').text + ".")
+	
 	if article.at_xpath('MedlineCitation/Article/Journal/JournalIssue/Volume') then
 		file.puts("journal: " + "\"" + article.xpath('MedlineCitation/Article/Journal/Title').text + " " + "<b>" + article.xpath('MedlineCitation/Article/Journal/JournalIssue/Volume').text + "</b>" + ", " + article.xpath('MedlineCitation/Article/Pagination/MedlinePgn').text + " (" + article.xpath('MedlineCitation/Article/Journal/JournalIssue/PubDate/Year').text + ")\"")
 	else
 		file.puts("journal: " + "\"" + article.xpath('MedlineCitation/Article/Journal/Title').text + " " + article.xpath('MedlineCitation/Article/Pagination/MedlinePgn').text + " (" + article.xpath('MedlineCitation/Article/Journal/JournalIssue/PubDate/Year').text + ")\"")
 	end
+	
 	if File.exists? File.expand_path("", "assets/pdfs/papers/" + article.xpath('PubmedData/ArticleIdList/ArticleId[@IdType="pubmed"]').text + ".pdf") then
 		file.puts("pdf: /assets/pdfs/papers/" + article.xpath('PubmedData/ArticleIdList/ArticleId[@IdType="pubmed"]').text + ".pdf")
 	end
+	
 	file.puts("doi: " + article.xpath('PubmedData/ArticleIdList/ArticleId[@IdType="doi"]').text)
+	
 	if article.at_xpath('MedlineCitation/Article/Journal/JournalIssue/Volume') then
 		file.puts("abbrev: " + "\"" + article.xpath('MedlineCitation/Article/Journal/ISOAbbreviation').text + " " + "<b>" + article.xpath('MedlineCitation/Article/Journal/JournalIssue/Volume').text + "</b>" + ", " + article.xpath('MedlineCitation/Article/Pagination/MedlinePgn').text + " (" + article.xpath('MedlineCitation/Article/Journal/JournalIssue/PubDate/Year').text + ")\"")
 	else
 		file.puts("abbrev: " + "\"" + article.xpath('MedlineCitation/Article/Journal/ISOAbbreviation').text + " " + article.xpath('MedlineCitation/Article/Pagination/MedlinePgn').text + " (" + article.xpath('MedlineCitation/Article/Journal/JournalIssue/PubDate/Year').text + ")\"")
 	end
+	
 	file.puts("year: " + article.xpath('MedlineCitation/Article/Journal/JournalIssue/PubDate/Year').text)
 	file.puts("---")
 	file.puts("")
+
+	file.puts("<div data-badge-popover=\"right\" data-badge-type=\"medium-donut\" data-doi=\"" + article.xpath('PubmedData/ArticleIdList/ArticleId[@IdType="doi"]').text + "\" data-hide-no-mentions=\"true\" class=\"altmetric-embed\"></div>")
+	file.puts("")
+	
 	if article.at_xpath('MedlineCitation/Article/Abstract/AbstractText') then
 		file.puts("# Abstract")
 		file.puts("")
 		
 		abstracts = article.xpath('MedlineCitation/Article/Abstract/AbstractText')
+		
 		abstracts.each do |abstract|
 			file.puts(abstract.text)
 			file.puts("")	
